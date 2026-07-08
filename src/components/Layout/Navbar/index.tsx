be5a276d5221a71b965e6870ea3708bcc2a1eccd
MARIA ISABEL <Mariah22-bot>
2026-07-08 15:23:00 -0300

    feat: ajustando responsividade do navbar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -11,32 +11,36 @@ type NavbarProps = {
 export const NavBar = ({ isOpen, onToggleMenu, onCloseMenu, onHomeClick }: NavbarProps) => {
 
     return (
-        <nav className="fixed left-0 top-0 z-50 w-full">
-            <div className="flex w-full items-center justify-between px-2 pt-3">
+        <nav className="bg-[#EFD9C7]/80 md:bg-transparent lg:bg-transparent fixed z-50 w-full">
+
+            <div className="flex w-full items-center justify-between px-2 pt-2">
                 <button
                     type="button"
                     onClick={onHomeClick}
-                    className="rounded-full p-2 transition-transform duration-300 ease-out hover:scale-[1.15] active:scale-[0.97] focus:outline-none"
+                    className="rounded-full p-1 transition-transform duration-300 ease-out hover:scale-[1.15] active:scale-[0.97] focus:outline-none"
                     aria-label="Voltar para página principal"
                 >
                     <img
                         src={logoImg}
                         alt="Logo Mariah Desenvolvedora Front-end"
-                        className="h-auto w-28 object-contain cursor-pointer"
+                        className="mb-2 h-auto w-16 md:w-20 lg:w-20 object-contain cursor-pointer"
                     />
                 </button>
 
                 <button
                     type="button"
                     onClick={isOpen ? onCloseMenu : onToggleMenu}
-                    className="rounded-full p-2 transition-transform duration-300 ease-out hover:scale-[1.15] active:scale-[0.97] focus:outline-none cursor-pointer botao-menu-sanduiche"
+                    className="rounded-full p-1 transition-transform duration-300 ease-out hover:scale-[1.15] active:scale-[0.97] focus:outline-none cursor-pointer botao-menu-sanduiche"
                     aria-label="Abrir menu"
                     aria-expanded={isOpen}
                 >
-                    <img src={menuImg} alt="Ícone de menu" className="h-auto w-20 object-contain pointer-events-none" />
+                    <img
+                        src={menuImg}
+                        alt="Ícone de menu"
+                        className="mb-2 h-auto w-11 md:w-15 lg:w-15 object-contain pointer-events-none"
+                    />
                 </button>
             </div>
-
         </nav>
     );
 };
