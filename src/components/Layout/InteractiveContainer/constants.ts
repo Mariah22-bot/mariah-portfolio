3c3c2c2d18c5cc45492e7cd00a74e93962c9a6c9
MARIA ISABEL <Mariah22-bot>
2026-08-04 12:11:17 -0300

    feat: deixando código mais organizado pra ficar mais legível.

--- src/components/Layout/InteractiveContainer/constants.ts
+++ src/components/Layout/InteractiveContainer/constants.ts
@@ -1,14 +1,8 @@
 /**
- * ==========================================================
  * HOTSPOT CONSTANTS
- * ==========================================================
- *
+
  * Este arquivo centraliza todas as constantes utilizadas
  * pelos hotspots.
- *
- * Nenhum componente deve possuir números "mágicos".
- *
- * ==========================================================
  */
 
 /* ==========================================================
@@ -16,13 +10,9 @@
  * ========================================================== */
 
 export const COLORS = {
-
     eye: "var(--color-glow-eye)",
-
     hair: "var(--color-glow-hair)",
-
     mouth: "var(--color-glow-mouth)",
-
 } as const;
 
 /* ==========================================================
@@ -30,29 +20,19 @@ export const COLORS = {
  * ========================================================== */
 
 export const STROKE = {
-
     eye: {
-
         outline: 4,
-
         line: 4,
-
     },
 
     mouth: {
-
         outline: 4,
-
         line: 4,
-
     },
 
     hair: {
-
         outline: 4,
-
         clickArea: 40,
-
     },
 
 } as const;
@@ -62,17 +42,11 @@ export const STROKE = {
  * ========================================================== */
 
 export const OPACITY = {
-
     hidden: 0.2,
-
     default: 0.4,
-
     visible: 0.8,
-
     active: 0.9,
-
     hover: 1,
-
 } as const;
 
 /* ==========================================================
@@ -80,11 +54,8 @@ export const OPACITY = {
  * ========================================================== */
 
 export const ANIMATION = {
-
     duration: 500,
-
     hoverScale: 1.1,
-
 } as const;
 
 /* ==========================================================
@@ -92,7 +63,6 @@ export const ANIMATION = {
  * ========================================================== */
 
 export const CLASSES = {
-
     hotspot: `
         fill-none
         transition-all
